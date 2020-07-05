dc1a23952ae926f98d24c4376a322f38d31b6f0f
Shashank Lal <shashanklal01@gmail.com>
2020-07-05 23:07:49 +0400

    added placeholder home

---
+++ src/admin/AdminHome.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View, StyleSheet } from 'react-native'
+import { firebase } from '../firebase/config'
+
+export default function UserHome() {
+
+    return(
+            <Text>Admin Dashboard</Text>
+        )
+    
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View, StyleSheet } from 'react-native'
 import { firebase } from '../firebase/config'
+import UserHome from '../user/UserHome'
+import AdminHome from '../admin/AdminHome'
 
-export default function Home(props) {
-return (
-    <Text>Home Screen</Text>
-)
+export default function Home() {
+
+    const id = firebase.auth().currentUser.uid
+    const isAdmin = firebase.firestore().collection('users').doc(id)
+
+    return (
+        <View>
+            { isAdmin ? (
+                <UserHome />
+            ) : (
+                <AdminHome />
+            )}
+        </View>
+    )
 }
