2e994420f369eef15fad6ba5167b78d1c41eeeea
Shashank Lal <shashanklal01@gmail.com>
2020-07-06 21:12:01 +0400

    ui improvements

--- src/admin/AdminSetup.js
+++ src/admin/AdminSetup.js
@@ -7,29 +7,49 @@ import { Card } from 'react-native-elements'
 export default function AdminSetup() {
     return (
         <View>
-            <Card
-                title='Current Queue'
-                titleStyle={styles.titleSyle}
-            >
-                
-            </Card>
-            <Card
-                title='Manage Queue'
-                titleStyle={styles.titleSyle}
-            >
-                
-            </Card>
-            <Card
-                title='some other option'
-                titleStyle={styles.titleSyle}
-            >
-                
+            <Card containerStyle={styles.card}>
+                <Card
+                    containerStyle={styles.card}
+                    title='Current Queue'
+                    titleStyle={styles.titleSyle}
+                >
+
+                </Card>
+                <Card
+                    containerStyle={styles.card}
+                    title='Manage Queue'
+                    titleStyle={styles.titleSyle}
+                >
+
+                </Card>
+                <Card
+                    containerStyle={styles.card}
+                    title='some other option'
+                    titleStyle={styles.titleSyle}
+                >
+
+                </Card>
             </Card>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    card: {
+        borderRadius: 20,
+        elevation: 3,
+        backgroundColor: '#fff',
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: '#333',
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        marginHorizontal: 4,
+        marginVertical: 6,
+    },
+    cardContent: {
+        marginHorizontal: 18,
+        marginVertical: 20,
+    },
     titleStyle: {
         textAlign: "center",
         position: "absolute",
