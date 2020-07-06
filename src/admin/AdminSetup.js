58d2d4bf7e9d45ce429d4fe45b8dc7a549965bc9
Shashank Lal <shashanklal01@gmail.com>
2020-07-06 19:50:36 +0400

    admin ui

--- src/admin/AdminSetup.js
+++ src/admin/AdminSetup.js
@@ -2,10 +2,36 @@ import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View, StyleSheet, Picker } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { firebase } from '../firebase/config'
-import { Card } from '@paraboly/react-native-card'
+import { Card } from 'react-native-elements'
 
 export default function AdminSetup() {
     return (
-        <Text>setup</Text>
+        <View>
+            <Card
+                title='Current Queue'
+                titleStyle={styles.titleSyle}
+            >
+                
+            </Card>
+            <Card
+                title='Manage Queue'
+                titleStyle={styles.titleSyle}
+            >
+                
+            </Card>
+            <Card
+                title='some other option'
+                titleStyle={styles.titleSyle}
+            >
+                
+            </Card>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    titleStyle: {
+        textAlign: "center",
+        position: "absolute",
+    },
+})
